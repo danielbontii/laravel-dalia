4c7289b59e0b746af4ffbda41f3bb90baaa3f3f6
adzamkomladev <adzamkomla.dev@gmail.com>
2020-06-29 19:44:24 +0000

    install dependencies

--- public/js/app.js
+++ public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Laravel Projects\laravel-dalia\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Laravel Projects\laravel-dalia\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/kbee/Documents/Projects/Learning/Laravel/laravel-dalia/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/kbee/Documents/Projects/Learning/Laravel/laravel-dalia/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
