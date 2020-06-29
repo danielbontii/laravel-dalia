e1c3ca5ab762af404ea80e0b1c01c4b683f2a3a8
adzamkomladev <adzamkomla.dev@gmail.com>
2020-06-29 20:28:03 +0000

    add a little UI fles

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-{{ $user->profile }}
+<div class="text-center w-100">
+    <img class="img-fluid rounded" src="//zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" alt="Avatar" width="100">
+</div>
+<div class="row w-100">
+    <div class="col-md-6">
+        <span class="font-weight-bold">Email:</span>
+        <span>{{ $user->email }}</span>
+    </div>
+    <div class="col-md-6">
+        <span class="font-weight-bold">Role:</span>
+        <span>{{ $user->role }}</span>
+    </div>
+</div>
 @endsection
