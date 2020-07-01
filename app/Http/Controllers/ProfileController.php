7ab130e5a7ca2bdfc2662c41dfa2e88fab5ca6e7
adzamkomladev <adzamkomla.dev@gmail.com>
2020-07-01 10:08:34 +0000

    add update profile method in controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateProfile;
 use Illuminate\Http\Request;
 use App\User;
 
@@ -27,5 +28,31 @@ class ProfileController extends Controller
     public function edit(User $user)
     {
         return view('profiles.edit', ['user' => $user]);
+    }
+
+    /**
+     * Update the given user.
+     *
+     * @param  UpdateProfile  $request
+     * @param  User $user
+     * @return Response
+     */
+    public function update(UpdateProfile $request, $user)
+    {
+        $validated = $request->validated();
+
+        $user->update([
+            'name' => $validated['name'] ?? $user->name,
+            'email' => $validated['email'] ?? $user->email,
+            'role' => $validated['role'] ?? $user->role,
+        ]);
+
+        $user->profile()->update([
+            'date_of_birth' => $validated['date_of_birth'] ?? $user->profile->date_of_birth,
+            'gender' => $validated['gender'] ?? $user->profile->gender,
+            'position' => $validated['position'] ?? $user->profile->position,
+        ]);
+
+        return redirect()->route('profiles.show');
     }
 }
